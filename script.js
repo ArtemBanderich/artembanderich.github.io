aa98cab835fdb2f8b88642f3147619549e38ba3f
ArtemBanderich <tezem777@gmail.com>
2021-12-03 17:39:24 +0200

    добавил всен а гит

---
+++ script.js
@@ -0,0 +1,10 @@
+//let number = 3;
+//console.log('string' * 7);
+
+const obj = {
+    Name: "Intel i5 10400",
+    Core: 6,
+    HyperTraiding: true,
+};
+console.log(obj.HyperTraiding);
+console.log(2 + +`2`);
